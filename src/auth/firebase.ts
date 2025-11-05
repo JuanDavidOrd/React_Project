5a07f25c684d165520a7cee1fd94ee9ab65c995e
JuanDavidOrd <juand71203@gmail.com>
2025-11-05 12:07:09 -0500

    Correccion FireBase

--- src/auth/firebase.ts
+++ src/auth/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, GithubAuthProvider, OAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAfi7DPkNIIqznaX14dJj6acXlWsIFztDs",
+  authDomain: "react-project-94d3b.firebaseapp.com",
+  projectId: "react-project-94d3b",
+  storageBucket: "react-project-94d3b.firebasestorage.app",
+  messagingSenderId: "150276229717",
+  appId: "1:150276229717:web:c4338c5e93ab86e807a2dc",
+  measurementId: "G-41H69BFGPW",
 };
 
 export const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
